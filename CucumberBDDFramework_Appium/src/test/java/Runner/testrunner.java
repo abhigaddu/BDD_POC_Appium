06ccf4101221e75b63f6a5547c5683a9a7bdb17f
abhigaddu <68762805+abhigaddu@users.noreply.github.com>
2020-08-27 17:45:39 +0530

    Update testrunner.java

--- CucumberBDDFramework_Appium/src/test/java/Runner/testrunner.java
+++ CucumberBDDFramework_Appium/src/test/java/Runner/testrunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/main/java/Feature/login.feature", glue = { "stepDefinitions" }, plugin = {
+@CucumberOptions(features = "src/main/java/Feature", glue = { "stepDefinitions" }, plugin = {
 		"html:target/cucumber-html-report", "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
 		"junit:target/cucumber-results.xml" })
 
